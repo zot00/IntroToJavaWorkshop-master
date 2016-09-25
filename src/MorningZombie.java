3fc3caa625a7a88c970ea96439d85d466c9c812c
TJHL <league@WTS9.attlocal.net>
2016-09-24 17:30:50 -0700

    I had fun
    dh8suadjkskdfjkij

---
+++ src/MorningZombie.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class MorningZombie {
+	public static void main(String[] args) {
+		while(true){
+ JOptionPane.showMessageDialog(null, "Hi! I have coffeeeeeurrrrrr");
+ JOptionPane.showMessageDialog(null, "By the time this ends, you will be what I am!");
+		}
+	}
+}
--- src/day3/StephenHawking.java
+++ src/day3/StephenHawking.java
@@ -10,12 +10,14 @@ public class StephenHawking {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10000; i++) {
 	String answer = JOptionPane.showInputDialog("Please text in here");
+	while(true){
 		speak(answer);
+	}
 	}
 	// 2. ask the user for a sentence
 	// 3. call the speak method below and send it the sentence
 	// 4. repeat steps 2 and 3 a lot of times
-	}
+		}
 	/* Don’t change this…. */
 	static void speak(String words) {
 		try {
@@ -24,5 +26,4 @@ public class StephenHawking {
 			e.printStackTrace();
 		}
 	}
-
 }
